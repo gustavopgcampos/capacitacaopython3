18b219a6cf452d2c3838d0531dd137e6cf8daf75
gustavopgcampos <gustavopgcampos@gmail.com>
2025-11-14 12:43:27 -0300

    feat: initial commit for project

---
+++ biblioteca/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'biblioteca'
+
+urlpatterns = [
+    path('', views.biblioteca, name='home'), 
+    path('livros/', views.listar_livros, name='listar_livros'), 
+    path('emprestimo/', views.criar_emprestimo, name='criar_emprestimo'), 
+    path('emprestimos/<int:id>/devolver/', views.devolver_livro, name='devolver_livro'),
+]
---
+++ users/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
+
+def register (request):
+
+    if request.method == "GET":
+        return render(request, 'register.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('password')
+
+        emailExists = User.objects.filter(email=email).first()
+    
+        if emailExists: 
+            return HttpResponse('email já cadastrado')
+        
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        user.save()
+
+        return HttpResponse('Usuário cadastrado com sucesso')
+
+def login (request): 
+
+    if request.method == "GET": 
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('password')
+
+        user = authenticate(username=username, password=senha)
+
+        if user: 
+            login_django(request, user)
+
+            return redirect('biblioteca:home')
+        else: 
+            return HttpResponse('Email ou senha inválidos')
